eb0b571268707e688bce71a233b599b388f027a8
Boukri Amine <AMINE_BOUKRI@emsi-edu.ma>
2023-11-10 10:25:21 +0100

    initial commit

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,4 +1,5 @@
 import {ReactNode} from "react";
+import {cn} from "@/lib/utils";
 
 const MaxWidthWrapper=({
     className,
@@ -7,7 +8,7 @@ const MaxWidthWrapper=({
     className?:string
     children:ReactNode
 })=>(
-    <div className='mx-auto w-full max-w-screen-xl px-2.5 md:px-20'>
+    <div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20',className)}>
         {children}
     </div>
 )
