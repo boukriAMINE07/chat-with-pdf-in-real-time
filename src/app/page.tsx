7d88623261bf5fc804fc95e80bb3105af1ed7302
Boukri Amine <AMINE_BOUKRI@emsi-edu.ma>
2023-11-10 11:23:40 +0100

    add a chadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,29 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from "next/link";
+import {buttonVariants} from "@/components/ui/button";
+import {ArrowRight} from "lucide-react";
 
 export default function Home() {
-  return (
-   <MaxWidthWrapper className={"mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center"}>
-     <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50 ">
-         <p className={'text-sm font-semibold text-gray-700'}>
-                <span >🚀</span> Quill is now publicly available!
-         </p>
-     </div>
-       <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-           Chat with your <span className="text-blue-600">documents</span> in real-time.
-       </h1>
-       <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-           Quill allows you to chat with your documents in real-time. It's like Google Docs, but with a chat window.
-       </p>
-   </MaxWidthWrapper>
-  )
+    return (
+        <>
+        <MaxWidthWrapper className={"mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center"}>
+            <div
+                className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50 ">
+                <p className={'text-sm font-semibold text-gray-700'}>
+                    <span>🚀</span> Quill is now publicly available!
+                </p>
+            </div>
+            <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+                Chat with your <span className="text-blue-600">documents</span> in real-time.
+            </h1>
+            <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+                Quill allows you to chat with your documents in real-time. It's like Google Docs, but with a chat
+                window.
+            </p>
+            <Link className={buttonVariants({size: 'lg', className: 'mt-5'})} href="/dashboard" target="_blank">
+                Get Started for Free <ArrowRight className={'ml-2 h-5 w-5'}/>
+            </Link>
+        </MaxWidthWrapper>
+        </>
+    )
 }
